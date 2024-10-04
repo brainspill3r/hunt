ff5dafede62cb66ec06c335e748607e0364fe1f4
root <root@vps.brainspiller>
2024-10-04 10:38:56 +0100

    Subdomain-Enumeration updated with discord webhooks

---
+++ SubdomainEnumeration/Utils/discordhooks.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// LoadEnv loads the .discordhooks.env file from a specified path
+func LoadEnv(filePath string) {
+	err := godotenv.Load(filePath)
+	if err != nil {
+		log.Fatalf("Error loading .discordhooks.env file: %v", err)
+	}
+}
+
+// GetScanCompletionWebhook retrieves the scan completion webhook from environment variables
+func GetScanCompletionWebhook() string {
+	webhook := os.Getenv("DISCORD_SCAN_COMPLETION_WEBHOOK")
+	if webhook == "" {
+		log.Fatalf("Error: DISCORD_SCAN_COMPLETION_WEBHOOK is not set in the environment variables")
+	}
+	return webhook
+}
+
+// GetPotentialTakeoverWebhook retrieves the potential takeover webhook from environment variables
+func GetPotentialTakeoverWebhook() string {
+	webhook := os.Getenv("DISCORD_POTENTIAL_TAKEOVER_WEBHOOK")
+	if webhook == "" {
+		log.Fatalf("Error: DISCORD_POTENTIAL_TAKEOVER_WEBHOOK is not set in the environment variables")
+	}
+	return webhook
+}
--- SubdomainEnumeration/go.mod
+++ SubdomainEnumeration/go.mod
@@ -3,3 +3,5 @@ module subdomain-enumeration
 go 1.22.5
 
 //require golang.org/x/sync v0.7.0
+
+require github.com/joho/godotenv v1.5.1
